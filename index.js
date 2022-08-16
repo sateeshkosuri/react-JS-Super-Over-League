abe1da44bc87b0d432f32a49ede5d97cabf41f31
Sateesh <sateeshkosuri1997@gmail.com>
2022-08-16 11:49:35 +0000

    Super Over League tests

---
+++ index.js
@@ -0,0 +1,19 @@
+const element = (
+  <div className="cardContainer">
+    <div className="bgContainer">
+      <h1 className="mainHeading">Super Over League</h1>
+      <div>
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/rcb-img.png"
+          className="image"
+        />
+        <img
+          src="https://assets.ccbp.in/frontend/react-js/csk-img.png"
+          className="image"
+        />
+      </div>
+    </div>
+  </div>
+);
+
+ReactDOM.render(element, document.getElementById("root"));
